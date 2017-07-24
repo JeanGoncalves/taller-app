712198d584ab5734015450838a57b66e85da36d3
Jean Gonçalves <jeanfpgoncalves@live.com>
2017-07-24 10:26:22 -0300

    # TEST # Node Express Route

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/', function(req, res) {
+    res.send('/login');
+    console.log('Teste 1');
+});
+
+app.get('/admin', function(req, res) {
+    res.send('/admin');
+    console.log('Teste 2');
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
