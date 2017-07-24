4a5b1fa45e6fe67d5383b85bba60479db2573896
Jean Gonçalves <jeanfpgoncalves@live.com>
2017-07-24 10:41:16 -0300

    # TEST # Update redirect Node routes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function(req, res) {
-    res.redirect('/login');
+    res.redirect('/');
 });
 
 // Start the app by listening on the default
